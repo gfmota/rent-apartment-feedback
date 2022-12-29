1ffb161ca69e951206b4b37b7cc09be8eed6ea90
gfmota <gabrielfmota134@gmail.com>
2022-12-29 15:35:11 -0300

    feat: add answers post
    Signed-off-by: gfmota <gabrielfmota134@gmail.com>

--- src/hooks/useFeedbackQuestions.ts
+++ src/hooks/useFeedbackQuestions.ts
@@ -14,10 +14,23 @@ export const useFeedbackQuestions = () => {
     [setComment]
   );
 
+  const sendAnswers = useCallback(() => {
+    const postData = async () =>
+      fetch(process.env.REACT_APP_FEEDBACK_API || '', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ date: new Date().toDateString(), comment, rate })
+      });
+    postData();
+  }, [comment, rate]);
+
   return {
     rate,
     onRateChange,
     comment,
-    onCommentChange
+    onCommentChange,
+    sendAnswers
   };
 };
--- src/hooks/useFeedbackSteps.ts
+++ src/hooks/useFeedbackSteps.ts
@@ -1,17 +1,20 @@
 import { useCallback } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useFeedbackContext } from '../state/FeedbackContext';
+import { useFeedbackQuestions } from './useFeedbackQuestions';
 
 export const STEPS_LENGTH = 3;
 
 export const useFeedbackSteps = () => {
   const { step, setStep } = useFeedbackContext();
+  const { sendAnswers } = useFeedbackQuestions();
   const navigate = useNavigate();
 
   const handleSubmit = useCallback(() => {
+    sendAnswers();
     navigate('/checkout');
     return null;
-  }, [navigate]);
+  }, [navigate, sendAnswers]);
 
   const onContinue = useCallback(() => {
     if (!step) return;
